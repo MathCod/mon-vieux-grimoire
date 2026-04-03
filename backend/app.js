05fa012cfb3e164a2e8ff71ed198cb97ec1f2223
Mathias BERGER <mathias.codage@gmail.com>
2026-04-03 18:32:32 +0200

    🚀🐍 Visibilité des images

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 const app = express();
 
@@ -26,6 +27,6 @@ app.use((req, res, next) => {
 
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
-app.use('/images', express.static('images'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
